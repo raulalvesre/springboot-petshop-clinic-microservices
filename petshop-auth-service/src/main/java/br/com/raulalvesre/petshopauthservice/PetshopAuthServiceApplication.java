95154a1f438f5c330e23d7cf01eeaa70347ccb8a
raul <raul.alves.re@gmail.com>
2022-10-28 16:25:18 -0300

    fix: fix `auth-service` application.properties

--- petshop-auth-service/src/main/java/br/com/raulalvesre/petshopauthservice/PetshopAuthServiceApplication.java
+++ petshop-auth-service/src/main/java/br/com/raulalvesre/petshopauthservice/PetshopAuthServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.raulalvesre.petshopauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PetshopAuthServiceApplication {
 
 	public static void main(String[] args) {
