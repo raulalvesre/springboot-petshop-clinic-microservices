02ea891cb0f7d81607776c5aeb65ba95ba928e9c
raul <raul.alves.re@gmail.com>
2023-01-10 17:59:58 -0300

    feat: add gateway

---
+++ petshop-gateway/src/main/java/br/com/raulalvesre/petshopgateway/PetshopGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.raulalvesre.petshopgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopGatewayApplication.class, args);
+	}
+
+}
---
+++ petshop-gateway/src/test/java/br/com/raulalvesre/petshopgateway/PetshopGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raulalvesre.petshopgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
