8982b9e2aca0556064e8696db1d68a692a6b75c4
raul <raul.alves.re@gmail.com>
2023-01-10 19:18:41 -0300

    eu devia ter comittado mais

--- petshop-visits-service/src/main/java/br/com/raulalvesre/petshopvisitsservice/dtos/VisitDto.java
+++ petshop-visits-service/src/main/java/br/com/raulalvesre/petshopvisitsservice/dtos/VisitDto.java
@@ -1,6 +1,10 @@
 package br.com.raulalvesre.petshopvisitsservice.dtos;
 
 import br.com.raulalvesre.petshopvisitsservice.models.Visit;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
