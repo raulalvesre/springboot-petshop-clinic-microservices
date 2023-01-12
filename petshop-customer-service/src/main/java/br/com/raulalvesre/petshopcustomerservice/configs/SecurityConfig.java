681c2dce458a1cea2e51335bd34c79fac1df1a37
raul <raul.alves.re@gmail.com>
2023-01-12 19:47:04 -0300

    fix: add protection to customer-service endpoints

--- petshop-customer-service/src/main/java/br/com/raulalvesre/petshopcustomerservice/configs/SecurityConfig.java
+++ petshop-customer-service/src/main/java/br/com/raulalvesre/petshopcustomerservice/configs/SecurityConfig.java
@@ -23,7 +23,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().anyRequest().permitAll();
+
+        http.authorizeRequests()
+                .antMatchers("/customer/email").permitAll()
+                .antMatchers( "/customer/**").hasAnyRole(ADMIN.name(), ATTENDANT.name())
+                .and().oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(authenticationConverter());
 
         return http.build();
     }
